baa658a94910bb6fa42bfb90254a7e9f6086147e
Mike Henry <82587+mhenry07@users.noreply.github.com>
2024-12-01 02:06:02 -0800

    Solve day 1 part 1

---
+++ Core/PoolableList.cs
@@ -0,0 +1,35 @@
+﻿using System.Buffers;
+
+namespace Core;
+
+public sealed class PoolableList<T> : IDisposable
+{
+    private T[] _buffer = ArrayPool<T>.Shared.Rent(16);
+    private int _length;
+
+    public void Add(T item)
+    {
+        if (_length == _buffer.Length)
+        {
+            ObjectDisposedException.ThrowIf(_buffer.Length == 0, this);
+
+            var old = _buffer;
+            _buffer = ArrayPool<T>.Shared.Rent(old.Length * 2);
+            old.AsSpan().CopyTo(_buffer);
+            ArrayPool<T>.Shared.Return(old);
+        }
+
+        _buffer[_length++] = item;
+    }
+
+    public int Length => _length;
+
+    public Span<T> Span => _buffer.AsSpan(0, _length);
+
+    public void Dispose()
+    {
+        ArrayPool<T>.Shared.Return(_buffer);
+        _buffer = [];
+        _length = 0;
+    }
+}
---
+++ Day01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Buffers.Text;
+using Core;
+
+//var testBytes = """
+//3   4
+//4   3
+//2   5
+//1   3
+//3   9
+//3   3
+//"""u8;
+
+var inputBytes = File.ReadAllBytes("input.txt");
+var bytes = inputBytes.AsSpan();
+var index = 0;
+var left = new PoolableList<int>();
+var right = new PoolableList<int>();
+foreach (var range in MemoryExtensions.Split(bytes, "\r\n"u8))
+{
+    var line = bytes[range];
+    if (line.Length == 0)
+        continue;
+
+    var separatorStart = line.IndexOf((byte)' ');
+    var separatorEnd = line.LastIndexOf((byte)' ');
+    if (separatorStart > 0 && separatorEnd > separatorStart
+        && Utf8Parser.TryParse(line[..separatorStart], out int l, out _)
+        && Utf8Parser.TryParse(line[(separatorEnd + 1)..], out int r, out _))
+    {
+        left.Add(l);
+        right.Add(r);
+    }
+    index++;
+}
+
+var leftSpan = left.Span;
+var rightSpan = right.Span;
+leftSpan.Sort();
+rightSpan.Sort();
+var totalDistance = 0;
+for (var i = 0; i < left.Length; i++)
+    totalDistance += Math.Abs(leftSpan[i] - rightSpan[i]);
+
+Console.WriteLine($"Total distance: {totalDistance:N0}");
