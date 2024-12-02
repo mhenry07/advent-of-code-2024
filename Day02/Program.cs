119505e371ab043cc50cf30ecfeb4943a37a4668
Mike Henry <82587+mhenry07@users.noreply.github.com>
2024-12-01 22:36:26 -0800

    Solve day 2 part 2

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -15,37 +15,26 @@ var bytes = useExample
     ? exampleBytes
     : File.ReadAllBytes("input.txt").AsSpan();
 var safeReports = 0;
+var dampenerSafeReports = 0;
 foreach (var lineRange in MemoryExtensions.Split(bytes, "\r\n"u8))
 {
     var line = bytes[lineRange];
     if (line.IsEmpty)
         continue;
 
-    var direction = Direction.Unknown;
-    var isSafe = default(bool?);
-    var lastLevel = default(int?);
-    foreach (var levelRange in MemoryExtensions.Split(line, (byte)' '))
+    if (IsSafe(line) is true)
     {
-        if (!Utf8Parser.TryParse(line[levelRange], out int level, out _))
-            throw new InvalidOperationException($"Failed to parse level from {Encoding.UTF8.GetString(line[levelRange])}");
-
-        if (!lastLevel.HasValue)
-        {
-            lastLevel = level;
-            continue;
-        }
-
-        isSafe = IsSafe(level, lastLevel, ref direction);
-        lastLevel = level;
-        if (isSafe is false)
-            break;
-    }
-
-    if (isSafe is true)
         safeReports++;
+        dampenerSafeReports++;
+    }
+    else if (IsSafeWithDampener(line) is true)
+    {
+        dampenerSafeReports++;
+    }
 }
 
 Console.WriteLine($"Safe reports: {safeReports}");
+Console.WriteLine($"Safe reports with Problem Dampener: {dampenerSafeReports}");
 
 enum Direction
 {
@@ -56,6 +45,71 @@ enum Direction
 
 static partial class Program
 {
+    static bool? IsSafe(ReadOnlySpan<byte> line)
+    {
+        var direction = Direction.Unknown;
+        var isSafe = default(bool?);
+        var lastLevel = default(int?);
+        foreach (var levelRange in MemoryExtensions.Split(line, (byte)' '))
+        {
+            if (!Utf8Parser.TryParse(line[levelRange], out int level, out _))
+                throw new InvalidOperationException($"Failed to parse level from {Encoding.UTF8.GetString(line[levelRange])}");
+
+            if (!lastLevel.HasValue)
+            {
+                lastLevel = level;
+                continue;
+            }
+
+            isSafe = IsSafe(level, lastLevel, ref direction);
+            lastLevel = level;
+            if (isSafe is false)
+                break;
+        }
+
+        return isSafe;
+    }
+
+    // naive implementation: O(N^2)
+    static bool IsSafeWithDampener(ReadOnlySpan<byte> line)
+    {
+        var length = 0;
+        for (var skip = 0; skip == 0 || skip < length; skip++)
+        {
+            var direction = Direction.Unknown;
+            var i = 0;
+            var isSafe = default(bool?);
+            var lastLevel = default(int?);
+            foreach (var levelRange in MemoryExtensions.Split(line, (byte)' '))
+            {
+                if (i++ == skip)
+                    continue;
+
+                if (!Utf8Parser.TryParse(line[levelRange], out int level, out _))
+                    throw new InvalidOperationException($"Failed to parse level from {Encoding.UTF8.GetString(line[levelRange])}");
+
+                if (!lastLevel.HasValue)
+                {
+                    lastLevel = level;
+                    continue;
+                }
+
+                isSafe = IsSafe(level, lastLevel, ref direction);
+                lastLevel = level;
+                if (isSafe is false)
+                    break;
+            }
+
+            if (isSafe is true)
+                return true;
+
+            if (skip == 0)
+                length = i;
+        }
+
+        return false;
+    }
+
     static bool IsSafe(int level, int? lastLevel, ref Direction reportDirection)
     {
         if (!lastLevel.HasValue)
