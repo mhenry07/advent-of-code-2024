22785223afd337b4135195e9c33689e0ad210245
Mike Henry <82587+mhenry07@users.noreply.github.com>
2024-12-02 23:15:16 -0800

    Solve day 3 part 2

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,16 +1,39 @@
 ﻿using System.Buffers.Text;
 
-var useExample = false;
-var exampleBytes = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"u8;
+int? useExample = null;
+var example1Bytes = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"u8;
+var example2Bytes = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"u8;
 
-var bytes = useExample
-    ? exampleBytes
-    : File.ReadAllBytes("input.txt").AsSpan();
+var bytes = useExample switch
+{
+    1 => example1Bytes,
+    2 => example2Bytes,
+    _ => File.ReadAllBytes("input.txt").AsSpan()
+};
 
-var total = 0;
+var @do = "do()"u8;
+var dont = "don't()"u8;
+var mul = "mul("u8;
+
+var enabled2 = true;
+var total1 = 0;
+var total2 = 0;
 for (var i = 0; i < bytes.Length; i++)
 {
-    var mul = "mul("u8;
+    if (bytes[i..].StartsWith(@do))
+    {
+        enabled2 = true;
+        i += @do.Length - 1;
+        continue;
+    }
+
+    if (bytes[i..].StartsWith(dont))
+    {
+        enabled2 = false;
+        i += dont.Length - 1;
+        continue;
+    }
+
     if (!bytes[i..].StartsWith(mul))
         continue;
 
@@ -30,10 +53,13 @@ for (var i = 0; i < bytes.Length; i++)
     if (!TryReadByte(bytes, i, out var closeParen) || closeParen != (byte)')')
         continue;
 
-    total += a * b;
+    total1 += a * b;
+    if (enabled2)
+        total2 += a * b;
 }
 
-Console.WriteLine($"Part 1 answer: {total}");
+Console.WriteLine($"Part 1 answer: {total1}");
+Console.WriteLine($"Part 2 answer: {total2}");
 
 static bool TryReadByte(ReadOnlySpan<byte> bytes, int index, out byte result)
 {
