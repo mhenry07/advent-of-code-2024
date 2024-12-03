b90e63cd78e46541e15ed9510e36c50c7f7b85f0
Mike Henry <82587+mhenry07@users.noreply.github.com>
2024-12-02 23:04:59 -0800

    Solve day 3 part 1

---
+++ Day03/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Buffers.Text;
+
+var useExample = false;
+var exampleBytes = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"u8;
+
+var bytes = useExample
+    ? exampleBytes
+    : File.ReadAllBytes("input.txt").AsSpan();
+
+var total = 0;
+for (var i = 0; i < bytes.Length; i++)
+{
+    var mul = "mul("u8;
+    if (!bytes[i..].StartsWith(mul))
+        continue;
+
+    i += mul.Length;
+    if (!Utf8Parser.TryParse(bytes[i..], out int a, out int aLength))
+        continue;
+
+    i += aLength;
+    if (!TryReadByte(bytes, i, out var comma) || comma != (byte)',')
+        continue;
+
+    i++;
+    if (!Utf8Parser.TryParse(bytes[i..], out int b, out int bLength))
+        continue;
+
+    i += bLength;
+    if (!TryReadByte(bytes, i, out var closeParen) || closeParen != (byte)')')
+        continue;
+
+    total += a * b;
+}
+
+Console.WriteLine($"Part 1 answer: {total}");
+
+static bool TryReadByte(ReadOnlySpan<byte> bytes, int index, out byte result)
+{
+    if (index >= bytes.Length - 1)
+    {
+        result = default;
+        return false;
+    }
+
+    result = bytes[index];
+    return true;
+}
