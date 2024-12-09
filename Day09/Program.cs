c1a989d245c467510c761f2266ec329f470d9fde
Mike Henry <82587+mhenry07@users.noreply.github.com>
2024-12-09 02:30:18 -0800

    Solve day 9 part 2

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -33,32 +33,28 @@ for (int i = 0, id = 0; i < diskMap.Length; i += 2, id++)
     }
 }
 
-var diskSpan = disk.Span;
-var contiguousLength = CompactFiles(diskSpan);
-var checksum1 = CalculateChecksumContiguous(diskSpan);
+var disk1 = disk.Span.ToArray().AsSpan();
+var contiguousLength1 = CompactFilesFragmenting(disk1);
+var checksum1 = CalculateChecksum(disk1);
+
+var disk2 = disk.Span.ToArray().AsSpan();
+CompactFilesNonFragmenting(disk2);
+var checksum2 = CalculateChecksum(disk2);
 
 var elapsed = TimeProvider.System.GetElapsedTime(start);
 
 Console.WriteLine($"Part 1 checksum: {checksum1}");
+Console.WriteLine($"Part 2 checksum: {checksum2}");
 Console.WriteLine($"Processed {bytes.Length:N0} bytes in: {elapsed.TotalMilliseconds:N3} ms");
 
-static void FormatDisk(StringBuilder builder, ReadOnlySpan<short?> disk)
-{
-    for (var i = 0; i < disk.Length; i++)
-    {
-        var item = disk[i];
-        builder.Append(item.HasValue ? (char)((item.Value % 10) + (byte)'0') : '.');
-    }
-}
-
-static long CalculateChecksumContiguous(Span<short?> disk)
+static long CalculateChecksum(Span<short?> disk)
 {
     var checksum = 0L;
     for (var i = 0; i < disk.Length; i++)
     {
         var id = disk[i];
         if (!id.HasValue)
-            break;
+            continue;
 
         checksum += i * id.Value;
     }
@@ -66,7 +62,7 @@ static long CalculateChecksumContiguous(Span<short?> disk)
     return checksum;
 }
 
-static int CompactFiles(Span<short?> disk)
+static int CompactFilesFragmenting(Span<short?> disk)
 {
     var builder = new StringBuilder(disk.Length);
     var i = 0;
@@ -79,14 +75,14 @@ static int CompactFiles(Span<short?> disk)
             continue;
         }
 
-        var block = disk[j];
-        if (IsFree(block))
+        var fileId = disk[j];
+        if (IsFree(fileId))
         {
             j--;
             continue;
         }
 
-        disk[i++] = block;
+        disk[i++] = fileId;
         disk[j--] = null;
 
         //FormatDisk(builder, disk);
@@ -97,6 +93,98 @@ static int CompactFiles(Span<short?> disk)
     return j + 1;
 }
 
+static void CompactFilesNonFragmenting(Span<short?> disk)
+{
+    var builder = new StringBuilder(disk.Length);
+    var i = 0;
+    var j = disk.Length - 1;
+    while (i < j)
+    {
+        if (!TryGetNextFreeBlock(disk, i, j, out i, out _))
+            break;
+
+        var fileId = disk[j];
+        if (IsFree(fileId))
+        {
+            j--;
+            continue;
+        }
+
+        var fileLength = GetFileLength(disk, j);
+        var fileStart = j - fileLength + 1;
+        j = fileStart - 1;
+        if (TryGetFreeBlock(disk, i, fileStart, fileLength, out var freeStart, out _))
+            MoveFile(disk, fileStart, fileLength, freeStart);
+        else
+            continue;
+
+        //FormatDisk(builder, disk);
+        //Console.WriteLine(builder);
+        //builder.Clear();
+    }
+}
+
+static int GetFileLength(ReadOnlySpan<short?> disk, int end)
+{
+    var id = disk[end];
+    if (IsFree(id))
+        return 0;
+
+    var length = 1;
+    for (var i = end - 1; i >= 0; i--)
+    {
+        if (disk[i] != id)
+            break;
+
+        length++;
+    }
+
+    return length;
+}
+
 static byte GetMapValue(byte utf8Byte) => (byte)(utf8Byte - (byte)'0');
 
 static bool IsFree(short? block) => !block.HasValue;
+
+static void MoveFile(Span<short?> disk, int fileStart, int fileLength, int freeStart)
+{
+    var originalFile = disk.Slice(fileStart, fileLength);
+    originalFile.CopyTo(disk[freeStart..]);
+
+    for (var i = 0; i < fileLength; i++)
+        originalFile[i] = null;
+}
+
+static bool TryGetFreeBlock(
+    ReadOnlySpan<short?> disk, int start, int end, int minSize, out int freeStart, out int freeLength)
+{
+    while (TryGetNextFreeBlock(disk, start, end, out freeStart, out freeLength))
+    {
+        if (freeLength >= minSize)
+            return true;
+
+        start = freeStart + freeLength;
+    }
+
+    freeStart = -1;
+    freeLength = 0;
+    return false;
+}
+
+static bool TryGetNextFreeBlock(ReadOnlySpan<short?> disk, int start, int end, out int freeStart, out int freeLength)
+{
+    freeStart = -1;
+    freeLength = 0;
+    for (var i = start; i < end; i++)
+    {
+        var id = disk[i];
+        if (freeStart == -1 && IsFree(id))
+            freeStart = i;
+        if (IsFree(id))
+            freeLength++;
+        else if (freeStart != -1)
+            break;
+    }
+
+    return freeStart != -1;
+}
