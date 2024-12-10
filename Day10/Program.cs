7101bd363373f0ae9b02eef17f162509f5fcd177
Mike Henry <82587+mhenry07@users.noreply.github.com>
2024-12-09 23:18:27 -0800

    Consolidate recursive score and rating methods

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -64,74 +64,54 @@ var bytes = useExample switch
 var mapData = MapData.FromInput(bytes, out var trailheadCollection, out var numPeaks);
 
 var reachablePeaks = new HashSet<Position>(numPeaks);
-var total1 = 0;
-var total2 = 0;
+var score = 0;
+var rating = 0;
 var trailheads = trailheadCollection.Span;
 foreach (var trailhead in trailheads)
 {
-    total1 += GetTrailScore(mapData, in trailhead, reachablePeaks);
-    total2 += GetTrailRating(mapData, in trailhead);
+    var (s, r) = GetTrailScoreAndRating(mapData, in trailhead, reachablePeaks);
+    score += s;
+    rating += r;
     reachablePeaks.Clear();
 }
 
 var elapsed = TimeProvider.System.GetElapsedTime(start);
 
-Console.WriteLine($"Part 1 answer: {total1}");
-Console.WriteLine($"Part 2 answer: {total2}");
+Console.WriteLine($"Part 1: Trailhead scores: {score}");
+Console.WriteLine($"Part 2: Trailhead ratings: {rating}");
 Console.WriteLine($"Processed {bytes.Length:N0} bytes in: {elapsed.TotalMilliseconds:N3} ms");
 
-static int GetTrailScore(MapData mapData, in TopoPosition position, HashSet<Position> reachablePeaks)
+static (int Score, int Rating) GetTrailScoreAndRating(
+    MapData mapData, in TopoPosition position, HashSet<Position> reachablePeaks)
 {
     if (position.Elevation == MapData.Peak)
-        return reachablePeaks.Add(position.Position) ? 1 : 0;
+        return (
+            Score: reachablePeaks.Add(position.Position) ? 1 : 0,
+            Rating: 1);
 
+    var rating = 0;
     var score = 0;
     var nextElevation = (byte)(position.Elevation + 1);
+    GetScoreAndRating(mapData, position.X, position.Y - 1, nextElevation, reachablePeaks, ref score, ref rating);
+    GetScoreAndRating(mapData, position.X, position.Y + 1, nextElevation, reachablePeaks, ref score, ref rating);
+    GetScoreAndRating(mapData, position.X + 1, position.Y, nextElevation, reachablePeaks, ref score, ref rating);
+    GetScoreAndRating(mapData, position.X - 1, position.Y, nextElevation, reachablePeaks, ref score, ref rating);
 
-    var north = position.Y - 1;
-    if (mapData.TryGetElevation(position.X, north, out var checkElevation) && checkElevation == nextElevation)
-        score += GetTrailScore(mapData, new TopoPosition(position.X, (sbyte)north, nextElevation), reachablePeaks);
+    return (score, rating);
 
-    var south = position.Y + 1;
-    if (mapData.TryGetElevation(position.X, south, out checkElevation) && checkElevation == nextElevation)
-        score += GetTrailScore(mapData, new TopoPosition(position.X, (sbyte)south, nextElevation), reachablePeaks);
+    static void GetScoreAndRating(
+        MapData mapData, int x, int y, byte nextElevation, HashSet<Position> reachablePeaks, ref int score,
+        ref int rating)
+    {
+        if (mapData.TryGetElevation(x, y, out var checkElevation) && checkElevation == nextElevation)
+        {
+            var (s, r) = GetTrailScoreAndRating(
+                mapData, new TopoPosition((sbyte)x, (sbyte)y, nextElevation), reachablePeaks);
 
-    var east = position.X + 1;
-    if (mapData.TryGetElevation(east, position.Y, out checkElevation) && checkElevation == nextElevation)
-        score += GetTrailScore(mapData, new TopoPosition((sbyte)east, position.Y, nextElevation), reachablePeaks);
-
-    var west = position.X - 1;
-    if (mapData.TryGetElevation(west, position.Y, out checkElevation) && checkElevation == nextElevation)
-        score += GetTrailScore(mapData, new TopoPosition((sbyte)west, position.Y, nextElevation), reachablePeaks);
-
-    return score;
-}
-
-static int GetTrailRating(MapData mapData, in TopoPosition position)
-{
-    if (position.Elevation == MapData.Peak)
-        return 1;
-
-    var score = 0;
-    var nextElevation = (byte)(position.Elevation + 1);
-
-    var north = position.Y - 1;
-    if (mapData.TryGetElevation(position.X, north, out var checkElevation) && checkElevation == nextElevation)
-        score += GetTrailRating(mapData, new TopoPosition(position.X, (sbyte)north, nextElevation));
-
-    var south = position.Y + 1;
-    if (mapData.TryGetElevation(position.X, south, out checkElevation) && checkElevation == nextElevation)
-        score += GetTrailRating(mapData, new TopoPosition(position.X, (sbyte)south, nextElevation));
-
-    var east = position.X + 1;
-    if (mapData.TryGetElevation(east, position.Y, out checkElevation) && checkElevation == nextElevation)
-        score += GetTrailRating(mapData, new TopoPosition((sbyte)east, position.Y, nextElevation));
-
-    var west = position.X - 1;
-    if (mapData.TryGetElevation(west, position.Y, out checkElevation) && checkElevation == nextElevation)
-        score += GetTrailRating(mapData, new TopoPosition((sbyte)west, position.Y, nextElevation));
-
-    return score;
+            rating += r;
+            score += s;
+        }
+    }
 }
 
 class MapData
