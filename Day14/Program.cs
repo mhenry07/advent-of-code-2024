7e21ad99185ffff530af11919642e3bb93c0aaf7
Mike Henry <82587+mhenry07@users.noreply.github.com>
2024-12-14 03:39:03 -0800

    Solve day 14 part 2
    - includes visualization and navigation

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -52,9 +52,9 @@ var lr = 0;
 var i = 0;
 foreach (var robot in robots.Span)
 {
-    Console.WriteLine($"Moving robot {i}");
+    //Console.WriteLine($"Moving robot {i}");
     var quadrant = robot.Move(in area, numSeconds);
-    Console.WriteLine($"  X: {robot.Position.X}, Y: {robot.Position.Y}, Quadrant: {quadrant}");
+    //Console.WriteLine($"  X: {robot.Position.X}, Y: {robot.Position.Y}, Quadrant: {quadrant}");
     switch (quadrant)
     {
         case Quadrant.UpperLeft:
@@ -76,11 +76,167 @@ foreach (var robot in robots.Span)
 
 var total1 = ul * ur * ll * lr;
 
+for (i = 0; !IsEasterEgg(i); i++)
+{ }
+var seconds2 = i;
+
 var elapsed = TimeProvider.System.GetElapsedTime(start);
 
+Console.WriteLine();
 Console.WriteLine($"Part 1: Safety factor: {total1}");
-//Console.WriteLine($"Part 2: Safety factor: {total2}");
+Console.WriteLine($"Part 2: Easter egg seconds: {seconds2}");
 Console.WriteLine($"Processed {bytes.Length:N0} input bytes in: {elapsed.TotalMilliseconds:N3} ms");
+Console.ReadLine();
+
+// interesting times:
+// - 49 seconds: a horizontal grouping lower down
+//   - also: 152, 255, 358, 461 (every 103)
+// - 98 seconds: a vertical grouping on the right
+//   - also: 199, 300, 401 (every 101)
+// - they combine when n = 103x + 49 = 101y + 98
+const char Background = '.';
+i = 0;
+var increment = 0;
+var navigationMode = NavigationMode.Pause;
+Span<char> buffer = new char[area.Width * area.Height];
+var lineBuffer = new char[area.Width];
+while (true)
+{
+    switch (navigationMode)
+    {
+        case NavigationMode.FastForward:
+        case NavigationMode.PageBackward:
+        case NavigationMode.PageForward:
+            if (i != 0 && !IsInterestingFrame(i))
+            {
+                i += increment;
+                continue;
+            }
+            break;
+        case NavigationMode.SkipToEasterEgg:
+            if (!IsEasterEgg(i))
+            {
+                i += increment;
+                continue;
+            }
+            break;
+    }
+
+    Console.WriteLine($"Seconds: {i}");
+    Console.WriteLine();
+    buffer.Fill(Background);
+
+    foreach (var robot in robots.Span)
+    {
+        robot.Move(in area, i);
+        ref char tile = ref buffer[GetIndex(in area, robot.Position.X, robot.Position.Y)];
+        tile = tile switch
+        {
+            Background => '1',
+            < '9' => (char)(tile + 1),
+            '9' => tile,
+            _ => tile
+        };
+    }
+
+    for (var y = 0; y < area.Height; y++)
+    {
+        buffer.Slice(GetIndex(in area, 0, y), area.Width).CopyTo(lineBuffer);
+        Console.WriteLine(lineBuffer);
+    }
+
+    if (IsEasterEgg(i))
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Easter egg found at {i}");
+        Thread.Sleep(2_000);
+        Console.Write("Press ENTER to continue navigating");
+        Console.ReadLine();
+    }
+
+    var shouldBreak = false;
+    if (navigationMode == NavigationMode.Play || navigationMode == NavigationMode.FastForward)
+    {
+        if (Console.KeyAvailable)
+        {
+            var keyInfo = Console.ReadKey();
+            if (keyInfo.Key == ConsoleKey.Q)
+            {
+                break;
+            }
+            else
+            {
+                increment = 0;
+                navigationMode = NavigationMode.Pause;
+            }
+        }
+        else
+        {
+            Thread.Sleep(250);
+        }
+    }
+    else
+    {
+        var keyInfo = Console.ReadKey();
+        switch (keyInfo.Key)
+        {
+            case ConsoleKey.Q:
+                shouldBreak = true;
+                increment = 0;
+                navigationMode = NavigationMode.Pause;
+                break;
+            case ConsoleKey.Spacebar:
+                increment = 1;
+                navigationMode = NavigationMode.Play;
+                break;
+            case ConsoleKey.F:
+                increment = 1;
+                navigationMode = NavigationMode.FastForward;
+                break;
+            case ConsoleKey.LeftArrow:
+                increment = -1;
+                navigationMode = NavigationMode.StepBackward;
+                break;
+            case ConsoleKey.RightArrow:
+                increment = 1;
+                navigationMode = NavigationMode.StepForward;
+                break;
+            case ConsoleKey.PageUp:
+                increment = -1;
+                navigationMode = NavigationMode.PageBackward;
+                break;
+            case ConsoleKey.PageDown:
+                increment = 1;
+                navigationMode = NavigationMode.PageForward;
+                break;
+            case ConsoleKey.Home:
+                i = 0;
+                increment = 0;
+                navigationMode = NavigationMode.Pause;
+                break;
+            case ConsoleKey.End:
+                increment = 1;
+                navigationMode = NavigationMode.SkipToEasterEgg;
+                break;
+        }
+    }
+
+    if (shouldBreak)
+        break;
+
+    i += increment;
+    Console.Clear();
+    buffer.Clear();
+}
+
+static int GetIndex(in Area area, int x, int y)
+    => y * area.Width + x;
+
+static bool IsEasterEgg(int seconds)
+    => (seconds - 49) % 103 == 0 && (seconds - 98) % 101 == 0;
+
+static bool IsInterestingFrame(int seconds)
+    => (seconds - 49) % 103 == 0 || (seconds - 98) % 101 == 0;
 
 static bool TryParsePair(
     ReadOnlySpan<byte> source, ReadOnlySpan<byte> prefix, out int x, out int y, out int bytesConsumed)
@@ -155,6 +311,18 @@ struct Robot(Position position, Velocity velocity)
         _position = new(x, y);
         return area.GetQuadrant(in _position);
     }
+}
+
+enum NavigationMode
+{
+    Pause,
+    Play,
+    FastForward,
+    StepBackward,
+    StepForward,
+    PageBackward,
+    PageForward,
+    SkipToEasterEgg
 }
 
 enum Quadrant
