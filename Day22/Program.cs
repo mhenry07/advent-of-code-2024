30a0cba94b69b5bc0772c751b427ee8775901f48
Mike Henry <82587+mhenry07@users.noreply.github.com>
2024-12-22 01:38:48 -0800

    Solve day 22 part 2 more efficiently
    - execution time: 419 ms

--- Day22/Program.cs
+++ Day22/Program.cs
@@ -29,6 +29,10 @@ const int NumRounds = 2_000;
 
 var j = 0;
 var total1 = 0L;
+var bestSequence = default(ChangeSequence);
+var monkeySeenSequences = new HashSet<ChangeSequence>();
+var sequenceBananas = new Dictionary<ChangeSequence, long>();
+var totalBananas = 0L;
 using var monkeyPrices = new PoolableList<int[]>();
 using var monkeyPriceChanges = new PoolableList<int[]>();
 foreach (var range in bytes.Split("\r\n"u8))
@@ -39,8 +43,10 @@ foreach (var range in bytes.Split("\r\n"u8))
 
     Utf8Parser.TryParse(line, out long initialSecretNumber, out _);
 
-    var prices = new int[2_001];
-    var priceChanges = new int[2_001];
+    var pricesArray = new int[2_001];
+    var priceChangesArray = new int[2_001];
+    var prices = pricesArray.AsSpan();
+    var priceChanges = priceChangesArray.AsSpan();
     prices[0] = (int)(initialSecretNumber % 10);
     priceChanges[0] = 0;
     var secretNumber = initialSecretNumber;
@@ -49,61 +55,39 @@ foreach (var range in bytes.Split("\r\n"u8))
         secretNumber = Next(secretNumber);
         var price = prices[i + 1] = (int)(secretNumber % 10);
         priceChanges[i + 1] = price - prices[i];
+
+        if (i >= 3)
+        {
+            var sequence = priceChanges.Slice(i - 2, 4);
+            var key = ChangeSequence.FromSpan(sequence);
+            if (!monkeySeenSequences.Add(key))
+                continue;
+
+            if (sequenceBananas.TryGetValue(key, out var bananas))
+            {
+                sequenceBananas[key] = bananas + price;
+                if (bananas + price > totalBananas)
+                {
+                    bestSequence = key;
+                    totalBananas = bananas + price;
+                }
+            }
+            else
+            {
+                sequenceBananas.Add(key, price);
+            }
+        }
     }
 
-    monkeyPrices.Add(prices);
-    monkeyPriceChanges.Add(priceChanges);
+    monkeyPrices.Add(pricesArray);
+    monkeyPriceChanges.Add(priceChangesArray);
+    monkeySeenSequences.Clear();
 
     total1 += secretNumber;
     j++;
 }
 
-Console.WriteLine($"Part 1 elapsed: {TimeProvider.System.GetElapsedTime(start)}");
-
-Span<int> bestSequence = [];
-var checkedSequences = new Dictionary<ChangeSequence, long>();
-var totalBananas = 0L;
-var monkeyPricesSpan = monkeyPrices.Span;
-var monkeyPriceChangesSpan = monkeyPriceChanges.Span;
-
-// the best sequence may not exist in the first monkey's price changes,
-// but if a sequence is missing from too many monkeys, it will be highly unlikely to yield the most bananas
-for (var l = 0; l < monkeyPricesSpan.Length / 2; l++)
-{
-    // check all possible sequences for each monkey
-    for (var k = 0; k <= NumRounds - 3; k++)
-    {
-        var bananas = 0L;
-        var sequence = monkeyPriceChanges[l].AsSpan(k, 4);
-        var key = ChangeSequence.FromSpan(sequence);
-        if (checkedSequences.ContainsKey(key))
-            continue;
-
-        // count bananas for the sequence across all monkeys
-        for (var i = 0; i < monkeyPricesSpan.Length; i++)
-        {
-            var prices = monkeyPricesSpan[i].AsSpan();
-            var priceChanges = monkeyPriceChangesSpan[i].AsSpan();
-
-            var changeIndex = priceChanges.IndexOf(sequence);
-            if (changeIndex >= 0 && changeIndex <= NumRounds - 3)
-            {
-                bananas += prices[changeIndex + 3];
-            }
-        }
-
-        checkedSequences.Add(key, bananas);
-        if (bananas > totalBananas)
-        {
-            totalBananas = bananas;
-            bestSequence = sequence;
-        }
-    }
-
-    Console.WriteLine($"Best bananas after monkey {l}: bananas: {totalBananas}, sequence: {string.Join(',', bestSequence.ToArray())}, elapsed: {TimeProvider.System.GetElapsedTime(start).TotalMilliseconds} ms");
-}
-
-Console.WriteLine($"Best sequence: {string.Join(',', bestSequence.ToArray())}, total bananas: {totalBananas}");
+Console.WriteLine($"Best sequence: {bestSequence}, total bananas: {totalBananas}");
 
 var elapsed = TimeProvider.System.GetElapsedTime(start);
 
@@ -141,4 +125,6 @@ record struct ChangeSequence(sbyte A, sbyte B, sbyte C, sbyte D)
 
     public readonly bool Equals(ReadOnlySpan<int> other)
         => A == other[0] && B == other[1] && C == other[2] && D == other[3];
+
+    public readonly override string ToString() => $"[{A},{B},{C},{D}]";
 }
